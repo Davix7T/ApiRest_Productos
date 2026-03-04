9918fee57cd1ac6a854b597ef7439dce372ffd79
Davix7T <142359414+Davix7T@users.noreply.github.com>
2026-03-04 08:40:56 -0500

    Add files via upload

---
+++ products/src/main/java/com/store/products/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.store.products.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API REST PARA PRODUCTOS";
+    }
+}   
